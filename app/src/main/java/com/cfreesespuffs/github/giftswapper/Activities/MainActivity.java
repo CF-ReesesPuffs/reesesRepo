b0790f7525421ebf1a7a724777fcda5e20b72908
claudiobailon <c.bailonschubert@gmail.com>
2020-11-17 13:31:50 -0800

    Amplify config added

--- app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/MainActivity.java
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/MainActivity.java
@@ -6,10 +6,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.analytics.pinpoint.AWSPinpointAnalyticsPlugin;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.auth.options.AuthSignOutOptions;
+import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Party;
 import com.cfreesespuffs.github.giftswapper.InvitedPartyPage;
 import com.cfreesespuffs.github.giftswapper.Adapters.PartyAdapter;
@@ -20,12 +30,15 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements PartyAdapter.InteractWithPartyListener{
     public ArrayList<Party> parties;
+    Handler handlecheckLoggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        configureAws();
+        getIsSignedIn();
         //=========== RecyclerView=======================
         RecyclerView partyRecyclerView = findViewById(R.id.party_recyclerview);
         partyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -63,8 +76,8 @@ public class MainActivity extends AppCompatActivity implements PartyAdapter.Inte
             MainActivity.this.startActivity(goToLoginIntent);
         });
 
-        ImageButton logoutButton= MainActivity.this.findViewById(R.id.nav_button);
-        navButton.setOnClickListener((view)-> {
+        ImageButton logoutButton= MainActivity.this.findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener((view)-> {
             logoutButton.setVisibility(View.INVISIBLE);
 
             Amplify.Auth.signOut(
@@ -75,8 +88,49 @@ public class MainActivity extends AppCompatActivity implements PartyAdapter.Inte
             );
         });
 //==============================================================================
+        handlecheckLoggedIn = new Handler(Looper.getMainLooper(), message -> {
+            if (message.arg1 == 0) {
+                Log.i("Amplify.login", "They weren't logged in");
+                ImageButton profile = MainActivity.this.findViewById(R.id.profile_button);
+                profile.setVisibility(View.INVISIBLE);
+            } else if (message.arg1 == 1) {
+                Log.i("Amplify.login", Amplify.Auth.getCurrentUser().getUsername());
+            } else {
+                Log.i("Amplify.login", "Send true or false pls");
+            }
+            return false;
+        });
 
+    }
 
+    public boolean getIsSignedIn() {
+        boolean[] isSingedIn = {false};
+        Amplify.Auth.fetchAuthSession(
+                result -> {
+                    Log.i("Amplify.login", result.toString());
+                    Message message = new Message();
+                    if (result.isSignedIn()) {
+                        message.arg1 = 1;
+                        handlecheckLoggedIn.sendMessage(message);
+                    } else {
+                        message.arg1 = 0;
+                        handlecheckLoggedIn.sendMessage(message);
+                    }
+                },
+                error -> Log.e("Amplify.login", error.toString())
+        );
+        return isSingedIn[0];
+    }
+    private void configureAws() {
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+//            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.addPlugin(new AWSPinpointAnalyticsPlugin(getApplication()));
+            Amplify.configure(getApplicationContext());
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
     }
 
 
