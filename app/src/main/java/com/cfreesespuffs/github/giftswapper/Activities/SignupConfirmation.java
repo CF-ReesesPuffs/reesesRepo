85564ebe649371152ee012f584b5ce080caed4fd
Paul OBrien <PVOVideo@outlook.com>
2020-11-18 14:07:12 -0800

    trying to resolve git's mess

--- app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/SignupConfirmation.java
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/SignupConfirmation.java
@@ -23,6 +23,8 @@ public class SignupConfirmation extends AppCompatActivity {
     Handler signUpHandler;
     Message message = new Message();
 
+    Handler signUpHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,41 +40,39 @@ public class SignupConfirmation extends AppCompatActivity {
 
 
             Amplify.Auth.confirmSignUp(
-                usernameConfirm.getText().toString().toLowerCase(),
-                confirmCode.getText().toString(),
-                result -> {
-                    Log.i("Amplify.confirm", result.isSignUpComplete() ? "Signup: Successful" : "Signup: FAIL"); // TODO: something better needs to happen here?
-                    message.arg1 = 123; // Todo: might need to create handler here.
-                    signUpHandler.sendEmptyMessage(message.arg1);
+                    usernameConfirm.getText().toString().toLowerCase(),
+                    confirmCode.getText().toString(),
+                    result -> {
+                        Log.i("Amplify.confirm", result.isSignUpComplete() ? "Signup: Successful" : "Signup: FAIL"); // TODO: something better needs to happen here?
+                        message.arg1 = 123; // Todo: might need to create handler here.
+                        signUpHandler.sendEmptyMessage(message.arg1);
 
-                    User newUser = User.builder()
-                            .userName(username)
-                            .build();
+                        User newUser = User.builder()
+                                .userName(username)
+                                .build();
 
-                    Amplify.API.mutate(
-                            ModelMutation.create(newUser),
-                            // TODO: add in email here.
-                            response -> Log.i("Amplify.API", "success"),
-                            error -> Log.e("Amplify.API", "newUser not created: " + error)
-                    );
+                        Amplify.API.mutate(
+                                ModelMutation.create(newUser),
+                                // TODO: add in email here.
+                                response -> Log.i("Amplify.API", "success"),
+                                error -> Log.e("Amplify.API", "newUser not created: " + error)
+                        );
 
-                    Amplify.Auth.signIn(
-                            username.toLowerCase(),
-                            password,
-                            loginResult -> this.startActivity(new Intent(SignupConfirmation.this, MainActivity.class)),
-                            thisError -> Log.e("Auth.Result", "Fail")
-                    );
-                },
-                error -> Log.e("Auth.Result", "failure")
-            )
-        ;
+                        Amplify.Auth.signIn(
+                                username.toLowerCase(),
+                                password,
+                                loginResult -> this.startActivity(new Intent(SignupConfirmation.this, MainActivity.class)),
+                                thisError -> Log.e("Auth.Result", "Fail")
+                        );
+                    },
+                    error -> Log.e("Auth.Result", "failure")
+            );
 
             Intent lastIntent = new Intent(SignupConfirmation.this, MainActivity.class);
             this.startActivity(lastIntent);
         });
 
         signUpHandler = new Handler(Looper.getMainLooper(), message -> {
-
             if (message.arg1 == 123) {
                 Context context = getApplicationContext();
                 CharSequence text = "User Confirmation Complete!";
