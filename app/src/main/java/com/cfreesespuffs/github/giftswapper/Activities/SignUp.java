e5d84bce2ba5ce3bebcfa34f554a6f0a526ac316
Paul OBrien <PVOVideo@outlook.com>
2021-01-29 11:57:43 -0800

    fix regex email pattern

--- app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/SignUp.java
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/SignUp.java
@@ -35,9 +35,12 @@ public class SignUp extends AppCompatActivity {
             EditText password = findViewById(R.id.passwordOneEt);
             EditText passwordTwo = findViewById((R.id.passwordTwoEt));
 
-            String regex = "^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$"; // https://howtodoinjava.com/java/regex/java-regex-validate-email-address/
+            String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"; // https://emailregex.com/
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(userEmail.getText().toString());
+
+            // TODO: ensure no one else has username (query the user list);
+            // TODO: if username already picked, create toast.
 
             if (matcher.matches()) {
                 Log.i("Amp.EmailMatcher", "The Email is Good! Clear!");
