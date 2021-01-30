d21ccb33248291d526337d7e09f9042204b739a0
Paul OBrien <PVOVideo@outlook.com>
2021-01-29 17:02:33 -0800

    correct editText focus route on login

--- app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/Login.java
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/Login.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,7 +15,10 @@ import android.preference.PreferenceManager;
 import android.text.method.KeyListener;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,7 +31,6 @@ public class Login extends AppCompatActivity {
 
     EditText username;
     EditText password;
-    Handler handleMismatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,43 @@ public class Login extends AppCompatActivity {
         username = findViewById(R.id.usernameLogin);
         password = findViewById(R.id.passwordLogin);
 
-        ((Button) findViewById(R.id.loginButton)).setOnClickListener(view -> {
+        username.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) { // https://stackoverflow.com/questions/20853560/how-to-move-to-next-input-field-when-enter-key-is-pressed-in-multiline-edittext
+                if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    username.clearFocus();
+                    password.requestFocus();
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(password, InputMethodManager.SHOW_IMPLICIT);
+                }
+                return false;
+            }
+        });
+
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    Log.e("Android.view", "BUMPT");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(password,InputMethodManager.SHOW_IMPLICIT);
+                }
+            }
+        });
+
+        password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) { // https://stackoverflow.com/questions/20853560/how-to-move-to-next-input-field-when-enter-key-is-pressed-in-multiline-edittext
+                if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    password.clearFocus();
+                    findViewById(R.id.loginButton).requestFocus();
+                }
+                return false;
+            }
+        });
+
+        findViewById(R.id.loginButton).setOnClickListener(view -> {
 
             if (username.getText().toString().equals("") || password.getText().toString().equals("")) {
                 Toast.makeText(this, "Fill in both fields please.", Toast.LENGTH_SHORT).show();
@@ -74,4 +113,5 @@ public class Login extends AppCompatActivity {
         });
 
     }
+
 }
