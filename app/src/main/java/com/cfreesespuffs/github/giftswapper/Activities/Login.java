f99c0affd70b50858128100153170d588afc5d37
Paul OBrien <PVOVideo@outlook.com>
2021-02-08 14:21:52 -0800

    add back in toast to login page (again)

--- app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/Login.java
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/Login.java
@@ -1,14 +1,23 @@
 package com.cfreesespuffs.github.giftswapper.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
@@ -17,10 +26,28 @@ import com.cfreesespuffs.github.giftswapper.R;
 
 public class Login extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        handler = new Handler(Looper.getMainLooper(),
+                new Handler.Callback() {
+                    @Override
+                    public boolean handleMessage(@NonNull Message msg) {
+                        if (msg.arg1 == 0) {
+                            Toast toast = Toast.makeText(getApplicationContext(),
+                                    "Please check your email and username are correct.", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            View toastView = toast.getView();
+                            toastView.getBackground().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN);
+                            toast.show();
+                        }
+                        return false;
+                    }
+                });
 
         ((Button) findViewById(R.id.loginButton)).setOnClickListener(view -> {
             EditText username = findViewById(R.id.usernameLogin);
@@ -50,7 +77,12 @@ public class Login extends AppCompatActivity {
                                 error -> Log.e("Amp.listByName", "error: " + error)
                         );
                     },
-                    error -> Log.e("Amplify.login", "Sign in fail: " + error.toString())
+                    error -> {
+                        Log.e("Amplify.login", "Sign in fail: " + error.toString());
+                        Message message = new Message();
+                        message.arg1 = 0;
+                        handler.sendMessage(message);
+                    }
             );
         });
 
