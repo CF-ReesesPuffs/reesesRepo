caa002917bd47f0f65c926f9616f8bf478a91e22
Paul OBrien <PVOVideo@outlook.com>
2021-04-12 08:52:44 -0700

    create activity and layout

---
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/FriendsPage.java
@@ -0,0 +1,16 @@
+package com.cfreesespuffs.github.giftswapper.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cfreesespuffs.github.giftswapper.R;
+
+public class FriendsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_page);
+    }
+}
