189a5578371e1fe31457d09cdad42e24ffcaf87a
Paul OBrien <PVOVideo@outlook.com>
2021-05-19 17:48:15 -0700

    save point redo invited_party_page

--- app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/InvitationDetails.java
+++ app/src/main/java/com/cfreesespuffs/github/giftswapper/Activities/InvitationDetails.java
@@ -61,6 +61,11 @@ public class InvitationDetails extends AppCompatActivity {
             if (msg.arg1 == 1) {
                 findViewById(R.id.declineInvite).setVisibility(View.VISIBLE);
             }
+
+            if (msg.arg1 ==2) {
+                TextView hostName = InvitationDetails.this.findViewById(R.id.partyHost);
+                hostName.setText(party.getTheHost().getUserName());
+            }
             return false;
         });
 
@@ -68,14 +73,16 @@ public class InvitationDetails extends AppCompatActivity {
                 ModelQuery.get(Party.class, partyId),
                 response -> {
                     party = response.getData();
-                    TextView hostName = InvitationDetails.this.findViewById(R.id.partyHost);
-                    hostName.setText(party.getTheHost().getUserName());
 
                     if (!preferences.getString("username", "NA").equals(party.getTheHost().getUserName())) {
                         Message message = new Message();
                         message.arg1 = 1;
                         handler.sendMessage(message);
                     }
+
+                    Message message = new Message();
+                    message.arg1 = 2;
+                    handler.sendMessage(message);
                 },
                 error -> Log.e("Amplify.query", "no party " + error)
         );
